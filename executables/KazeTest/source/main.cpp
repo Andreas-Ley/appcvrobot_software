750f43be2d866c8df2b4ef84180164b6b9d09809
Andreas Ley <mail@andreas-ley.com>
2021-03-27 00:06:11 +0100

    work on fast

--- executables/KazeTest/source/main.cpp
+++ executables/KazeTest/source/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d.hpp>
@@ -6,9 +5,15 @@
 #include <opencv2/highgui/highgui.hpp>
 
 
+#include <iostream>
 #include <time.h>
 #include <array>
 #include <stdint.h>
+#include <vector>
+#include <cstdint>
+#include <random>
+#include <tuple>
+#include <span>
 
 class CPUStopWatch
 {
@@ -46,7 +51,7 @@ uint64_t CPUStopWatch::getNanoseconds()
 }
 
 
-#if 0
+#ifndef BUILD_FOR_ARM
 
 template<typename type, unsigned dim>
 struct Vuint {
@@ -161,6 +166,16 @@ struct Vuint {
         return res;
     }
 
+    std::uint32_t extract32B(unsigned offset) const {
+        #if 0
+            const std::uint32_t *ptr = (const std::uint32_t *)&values;
+            return ptr[offset];
+        #else
+            std::uint32_t res;
+            memcpy(&res, ((const char*)values.data()) + offset * 4, 4);
+            return res;
+        #endif
+    }
 
     std::array<type, dim> values;
 };
@@ -316,6 +331,11 @@ struct Vuint8x16 {
         values = vsliq_n_u8(values, insert.values, shiftAmount);
     }
 
+    std::uint32_t extract32B(unsigned offset) const {
+        return vgetq_lane_u32((const uint32x4_t)values, offset);
+    }
+
+
     uint8x16_t values;
 };
 
@@ -453,7 +473,7 @@ void fast(const Image &img, Image &dst)
         {-1, -3},
     };
 
-    const unsigned t = 10;
+    const unsigned t = 5;
 
     for (unsigned y = 3; y+3 < img.height(); y++)
         for (unsigned x = 3; x+3 < img.width(); x+=16) {
@@ -553,12 +573,43 @@ void fast(const Image &img, Image &dst)
 
 }
 
-void nonMaxSuppress(Image &img)
+
+
+
+struct Keypoint {
+    std::uint16_t x;
+    std::uint16_t y;
+    std::uint8_t strength;
+};
+
+
+template<unsigned i>
+void emitCoords(unsigned x, unsigned y, const Vuint8x16 &centralPixel, std::vector<Keypoint> &coords)
 {
-    for (unsigned y = 1; y+1 < img.height(); y++)
-        for (unsigned x = 1; x+1 < img.width(); x+=16) {
+    auto fourPixels = centralPixel.extract32B(i);
+    if (fourPixels != 0) {
+        for (unsigned j = 0; j < 4; j++) {
+            std::uint8_t strength = (fourPixels >> (j*8)) & 0xFF;
+            if (strength) {
+                unsigned locX = x + i*4 + j;
+                coords.push_back({locX, y, strength});
+            }
+        }
+    }
+}
+
+template<bool writeBackImg>
+void nonMaxSuppress(Image &img, unsigned borderSize, std::vector<Keypoint> &coords)
+{
+    borderSize = std::max(borderSize, 1);
+
+    coords.clear();
+    for (unsigned y = borderSize; y+borderSize < img.height(); y++)
+        for (unsigned x = borderSize; x+borderSize < img.width(); x+=16) {
             Vuint8x16 centralPixel;
             centralPixel.load(&img(x, y));
+
+            if (!any(centralPixel)) continue;
 
             for (int i = -1; i <= 1; i++)
                 for (int j = -1; j <= 1; j++)
@@ -570,9 +621,266 @@ void nonMaxSuppress(Image &img)
                         centralPixel = centralPixel & larger;
                     }
 
-            centralPixel.store(&img(x, y));
+            if (writeBackImg)
+                centralPixel.store(&img(x, y));
+
+            if (any(centralPixel)) {
+                emitCoords<0>(x, y, centralPixel, coords);
+                emitCoords<1>(x, y, centralPixel, coords);
+                emitCoords<2>(x, y, centralPixel, coords);
+                emitCoords<3>(x, y, centralPixel, coords);
+            }
         }
 }
+
+const unsigned descriptorPatternMaxSize = 2048;
+const int descriptorPatternExtend = 24;
+std::tuple<int, int, int, int> descriptorPattern[descriptorPatternMaxSize];
+
+template<unsigned dimension = 256>
+void slowBrief(const Image &img,
+                const std::vector<std::tuple<std::uint16_t, std::uint16_t, std::uint8_t>> &coords,
+                std::vector<std::uint8_t> &descriptors,
+                std::vector<bool> &valid)
+{
+    unsigned descriptorWords = dimension / 8;
+    descriptors.resize(coords.size() * descriptorWords);
+    valid.resize(coords.size());
+    memset(descriptors.data(), 0, descriptors.size());
+
+    for (unsigned i = 0; i < coords.size(); i++) {
+        unsigned descOffset = i * descriptorWords;
+
+        auto [cx, cy, strength] = coords[i];
+        if (cx < descriptorPatternExtend || cx+descriptorPatternExtend >= img.width() ||
+            cy < descriptorPatternExtend || cy+descriptorPatternExtend >= img.height()) {
+            valid[i] = false;
+            continue;
+        }
+        valid[i] = true;
+
+        for (unsigned j = 0; j < dimension; j++) {
+            auto [sx, sy, dx, dy] = descriptorPattern[j];
+            bool bit = img(cx+sx, cy+sy) > img(cx+dx, cy+dy);
+            if (bit)
+                descriptors[descOffset+j/8] |= 1 << (j % 8);
+        }
+    }
+}
+
+
+template<unsigned dimension = 256>
+void slowMatch(const std::vector<std::uint8_t> &descriptorsA, const std::vector<std::uint8_t> &descriptorsB, std::vector<std::tuple<unsigned, unsigned>> &matches)
+{
+    unsigned numDescriptorsA = descriptorsA.size() / (dimension/8);
+    unsigned numDescriptorsB = descriptorsB.size() / (dimension/8);
+    const unsigned descWords = dimension / 32;
+    matches.resize(numDescriptorsA);
+    for (unsigned i = 0; i < numDescriptorsA; i++) {
+        if (i % 100 == 0)
+            std::cout << i << " of " << numDescriptorsA << std::endl;
+        const std::uint32_t *srcPtr = (const std::uint32_t *) &descriptorsA[i * (dimension/8)];
+
+        unsigned bestMatch = 0;
+        unsigned bestDistance = ~0u;
+
+        for (unsigned j = 0; j < numDescriptorsB; j++) {
+            const std::uint32_t *dstPtr = (const std::uint32_t *) &descriptorsB[j * (dimension/8)];
+
+            unsigned hammingDistance = 0;
+            for (unsigned k = 0; k < descWords; k++)
+                hammingDistance += __builtin_popcount(srcPtr[k] ^ dstPtr[k]);
+
+            if (hammingDistance < bestDistance) {
+                bestDistance = hammingDistance;
+                bestMatch = j;
+            }
+        }
+
+        matches[i] = {bestMatch, bestDistance};
+    }
+}
+
+
+
+class FrameKPGrid {
+    public:
+        void resize(unsigned rows, unsigned cols);
+
+        const std::span<unsigned> &operator()(unsigned r, unsigned c) const {
+            const auto &cell = m_cells[r*m_cols+c];
+            return std::span<unsigned>(m_keypointIndices.begin()+cell.idxStart, m_keypointIndices.begin()+cell.idxStart+cell.idxCount);
+        }
+
+        void build(unsigned width, unsigned height, const std::vector<Keypoint> &keypoints);
+
+        inline unsigned getRows() const { return m_rows; }
+        inline unsigned getCols() const { return m_cols; }
+    protected:
+        unsigned m_rows = 0;
+        unsigned m_cols = 0;
+
+        struct Cell {
+            unsigned idxStart = 0;
+            unsigned idxCount = 0;
+        };
+
+        std::vector<Cell> m_cells;
+        std::vector<unsigned> m_keypointIndices;
+};
+
+void FrameKPGrid::resize(unsigned rows, unsigned cols)
+{
+    m_rows = rows;
+    m_cols = cols;
+    m_cells.clear();
+    m_cells.resize(m_rows * m_cols);
+}
+
+void FrameKPGrid::build(unsigned width, unsigned height, const std::vector<Keypoint> &keypoints)
+{
+    m_cells.clear();
+    m_cells.resize(m_rows * m_cols);
+    m_keypointIndices.clear();
+
+    std::vector<std::pair<unsigned,unsigned>> kpRowCol;
+    kpRowCol.resize(keypoints.size());
+
+    for (unsigned i = 0; i < keypoints.size(); i++) {
+        unsigned r, c;
+        kpRowCol[i] = { r = keypoints[i].y * m_rows / height, c = keypoints[i].x * m_cols / width };
+        m_cells[r * m_cols + c].idxCount++;
+    }
+
+    {
+        unsigned cumulativeOffset = 0;
+        for (unsigned i = 0; i < m_cells.size(); i++) {
+            m_cells[i].idxStart = cumulativeOffset;
+            cumulativeOffset += m_cells[i].idxCount;
+        }
+    }
+
+    m_keypointIndices.resize(keypoints.size());
+    std::vector<unsigned> cellFillState(m_cells.size(), 0);
+    for (unsigned i = 0; i < keypoints.size(); i++) {
+        auto [row, col] = kpRowCol[i];
+        auto cellIdx = row * m_cols + col;
+
+        m_keypointIndices[m_cells[cellIdx].idxStart + cellFillState[cellIdx]] = i;
+        cellFillState[cellIdx]++;
+    }
+}
+
+
+
+
+struct RawMatch {
+    unsigned dstIdx;
+    std::uint16_t bestDistance;
+    std::uint16_t secondBestDistance;
+};
+
+class Frame {
+    public:
+        void extractKeypoints(const Image &img);
+        void buildKPGrid();
+
+        void matchWith(const Frame &other, std::vector<RawMatch> &dst);
+
+        unsigned getWidth() const { return m_width; }
+        unsigned getHeight() const { return m_height; }
+        const std::vector<Keypoint> &getKeypoints() const { return m_keypoints; }
+        const std::vector<std::uint8_t> &getDescriptors() const { return m_descriptors; }
+
+        const FrameKPGrid &getKPGrid() const { return m_kpGrid; }
+    protected:
+        unsigned m_width = 0;
+        unsigned m_height = 0;
+        std::vector<Keypoint> m_keypoints;
+        std::vector<std::uint8_t> m_descriptors;
+
+        FrameKPGrid m_kpGrid;
+};
+
+void Frame::extractKeypoints(const Image &img)
+{
+    m_width = img.width();
+    m_height = img.height();
+
+    m_keypoints.reserve(2000);
+
+    Image fastDetections;
+    fast(img, fastDetections);
+    nonMaxSuppress<false>(fastDetections, descriptorPatternExtend, m_keypoints);
+
+    std::vector<bool> valid;
+    slowBrief(img, m_keypoints, m_descriptors, valid);
+}
+
+void Frame::buildKPGrid()
+{
+    m_kpGrid.resize(m_height / 40, m_width / 40);
+    m_kpGrid.build(m_width, m_height, m_keypoints);
+}
+
+void Frame::matchWith(const Frame &other, std::vector<RawMatch> &dst)
+{
+    dst.resize(m_keypoints.size());
+    for (unsigned r = 0; r < m_kpGrid.getRows(); r++)
+        for (unsigned c = 0; c < m_kpGrid.getCols(); c++) {
+            auto cell = m_kpGrid(r, c);
+
+            for (auto srcIdx : cell) {
+                const auto srcKp = m_keypoints[srcIdx];
+                unsigned dstColx2 = srcKp.x * other.getKPGrid().getCols()*2 / other.getWidth();
+                unsigned dstRowx2 = srcKp.y * other.getKPGrid().getRows()*2 / other.getHeight();
+
+                int dstCol = dstColx2 / 2;
+                int dstRow = dstRowx2 / 2;
+                bool rightQuadrant = dstColx2 % 2;
+                bool bottomQuadrant = dstRowx2 % 2;
+
+                int startCol;
+                int endCol;
+                if (rightQuadrant) {
+                    startCol = (int)dstCol;
+                    endCol = (int)dstCol+1;
+                } else {
+                    startCol = (int)dstCol-1;
+                    endCol = (int)dstCol;
+                }
+
+                startCol = std::max(startCol, 0);
+                endCol = std::min(endCol, other.getKPGrid().getCols()-1);
+
+                int startRow;
+                int endRow;
+                if (bottomQuadrant) {
+                    startRow = (int)dstRow;
+                    endRow = (int)dstRow+1;
+                } else {
+                    startRow = (int)dstRow-1;
+                    endRow = (int)dstRow;
+                }
+
+                startRow = std::max(startRow, 0);
+                endRow = std::min(endRow, other.getKPGrid().getRows()-1);
+
+
+                for (unsigned r2 = startRow; r2 < endRow; r2++)
+                    for (unsigned c2 = startCol; c2 < endCol; c2++) {
+                        auto dstCell = other.getKPGrid()(r2, c2);
+
+
+                    }
+            }
+        }
+}
+
+
+
+
+
 
 void slow(const Image &img, Image &dst)
 {
@@ -595,7 +903,7 @@ void slow(const Image &img, Image &dst)
         {-1, -3},
     };
 
-    const unsigned t = 10;
+    const unsigned t = 5;
 
     for (unsigned y = 3; y+3 < img.height(); y++)
         for (unsigned x = 3; x+3 < img.width(); x++) {
@@ -639,7 +947,16 @@ void slow(const Image &img, Image &dst)
                 }
             }
 
-            dst(x, y) = isKeypoint?255:0;
+            if (isKeypoint) {
+                unsigned strength = 0;
+                for (unsigned tap = 0; tap < 16; tap++) {
+                    int tapPixel = img(x+taps[tap][0], y+taps[tap][1]);
+                    unsigned diff = std::abs(centralPixel - tapPixel);
+                    strength += diff;
+                }
+                dst(x, y) = strength;
+            } else
+                dst(x, y) = 0;
         }
 
 }
@@ -671,10 +988,108 @@ vuzp1q_s8
 
 int main() {
 
-#if 1
+    std::mt19937 mt;
+    std::normal_distribution<float> dist(0.0f, 8.0f);
+
+    for (unsigned i = 0; i < descriptorPatternMaxSize; i++) {
+        int sx = std::min<int>(std::max<int>(dist(mt), -descriptorPatternExtend), descriptorPatternExtend);
+        int sy = std::min<int>(std::max<int>(dist(mt), -descriptorPatternExtend), descriptorPatternExtend);
+        int dx = std::min<int>(std::max<int>(dist(mt), -descriptorPatternExtend), descriptorPatternExtend);
+        int dy = std::min<int>(std::max<int>(dist(mt), -descriptorPatternExtend), descriptorPatternExtend);
+        descriptorPattern[i] = {sx, sy, dx, dy};
+    }
+
+#ifdef BUILD_FOR_ARM
     auto imgA = cv::imread("/home/pi/frame0126.png", cv::IMREAD_GRAYSCALE);
-//    auto imgA = cv::imread("/home/andy/Documents/CAD/AppCVRobot/data/fullHDTest_autoIso_640_480_frames/frame0126.png", cv::IMREAD_GRAYSCALE);
-//    auto imgB = cv::imread("/home/andy/Documents/CAD/AppCVRobot/data/fullHDTest_autoIso_640_480_frames/frame0128.png");
+    auto imgB = cv::imread("/home/pi/frame0130.png", cv::IMREAD_GRAYSCALE);
+#else
+    auto imgA = cv::imread("/home/andy/Documents/CAD/AppCVRobot/data/fullHDTest_autoIso_640_480_frames/frame0126.png", cv::IMREAD_GRAYSCALE);
+    auto imgB = cv::imread("/home/andy/Documents/CAD/AppCVRobot/data/fullHDTest_autoIso_640_480_frames/frame0130.png", cv::IMREAD_GRAYSCALE);
+#endif
+
+#if 0
+
+
+
+    Image inputImg;
+    inputImg.allocate(imgA.cols, imgA.rows);
+    for (unsigned y = 0; y < imgA.rows; y++)
+        for (unsigned x = 0; x < imgA.cols; x++) {
+            inputImg(x, y) = imgA.at<std::uint8_t>(y, x);
+        }
+
+    Image outputImg;
+    outputImg.allocate(imgA.cols, imgA.rows);
+
+    std::vector<std::tuple<std::uint16_t, std::uint16_t, std::uint8_t>> coordsA;
+    coordsA.reserve(10000);
+
+    fast(inputImg, outputImg);
+    nonMaxSuppress<false>(outputImg, coordsA);
+
+    std::vector<std::uint8_t> descriptorsA;
+    std::vector<bool> validA;
+    slowBrief(inputImg, coordsA, descriptorsA, validA);
+
+
+    inputImg.allocate(imgB.cols, imgB.rows);
+    for (unsigned y = 0; y < imgB.rows; y++)
+        for (unsigned x = 0; x < imgB.cols; x++) {
+            inputImg(x, y) = imgB.at<std::uint8_t>(y, x);
+        }
+
+    outputImg.allocate(imgB.cols, imgB.rows);
+
+    std::vector<std::tuple<std::uint16_t, std::uint16_t, std::uint8_t>> coordsB;
+    coordsB.reserve(10000);
+
+    slow(inputImg, outputImg);
+    nonMaxSuppress<false>(outputImg, coordsB);
+
+    std::vector<std::uint8_t> descriptorsB;
+    std::vector<bool> validB;
+    slowBrief(inputImg, coordsB, descriptorsB, validB);
+
+
+
+    std::vector<std::tuple<unsigned, unsigned>> matches;
+    slowMatch(descriptorsA, descriptorsB, matches);
+
+    auto coords2keypoints = [](auto &coords, auto &keypoints) {
+        keypoints.resize(coords.size());
+        for (unsigned i = 0; i < coords.size(); i++) {
+            auto [x, y, s] = coords[i];
+            keypoints[i] = cv::KeyPoint({x, y}, 5);
+        }
+    };
+
+    std::vector<cv::KeyPoint> keypointsA;
+    coords2keypoints(coordsA, keypointsA);
+    std::vector<cv::KeyPoint> keypointsB;
+    coords2keypoints(coordsB, keypointsB);
+
+    std::vector<cv::DMatch> filteredMatches;
+    for (unsigned i = 0; i < matches.size(); i++) {
+        auto [idx, distance] = matches[i];
+        std::cout << idx << " " << distance << std::endl;
+        if (distance > 40) continue;
+        if (!validA[i]) continue;
+        //if (!validB[matches[i]]) continue;
+
+        filteredMatches.push_back(cv::DMatch(i, idx, distance));
+    }
+
+
+    cv::Mat outImage;
+    cv::drawMatches (imgA, keypointsA, imgB, keypointsB, filteredMatches, outImage);
+
+    cv::imwrite("matches_ours.png", outImage);
+#ifndef BUILD_FOR_ARM
+    cv::imshow("outImage", outImage);
+    cv::waitKey(0);
+#endif
+
+#elif 1
 
     //cv::cvtColor(imgA, cv::COLOR_BGR2GRAY);
 
@@ -688,10 +1103,22 @@ int main() {
     Image outputImg;
     outputImg.allocate(imgA.cols, imgA.rows);
 
+    std::vector<std::tuple<std::uint16_t, std::uint16_t, std::uint8_t>> coords;
+    coords.reserve(10000);
+
     CPUStopWatch timer;
     for (unsigned i = 0; i < 50; i++) {
         fast(inputImg, outputImg);
-        nonMaxSuppress(outputImg);
+        nonMaxSuppress<true>(outputImg, coords);
+    }
+    std::cout << timer.getNanoseconds() * 1e-9f / 50 << " seconds/image" << std::endl;
+
+
+    timer.start();
+    for (unsigned i = 0; i < 50; i++) {
+	    std::vector<std::uint8_t> descriptorsA;
+	    std::vector<bool> validA;
+	    slowBrief(inputImg, coords, descriptorsA, validA);
     }
     std::cout << timer.getNanoseconds() * 1e-9f / 50 << " seconds/image" << std::endl;
 
@@ -723,7 +1150,7 @@ int main() {
 //    cv::imshow("image", imgA);
 //    cv::waitKey(0);
 */
-#elif 1
+#elif 0
     //auto imgA = cv::imread("/home/pi/frame0126.png", cv::IMREAD_GRAYSCALE);
     auto imgA = cv::imread("/home/andy/Documents/CAD/AppCVRobot/data/fullHDTest_autoIso_640_480_frames/frame0126.png", cv::IMREAD_GRAYSCALE);
     auto fast = cv::FastFeatureDetector::create(21);
@@ -739,20 +1166,20 @@ int main() {
     cv::drawKeypoints(imgA, keypointsA, outImageA);
 
     cv::imwrite("opencv_fast.png", outImageA);
-#elif 0
+#elif 1
     cv::Mat descsA;
     std::vector<cv::KeyPoint> keypointsA;
     cv::Mat descsB;
     std::vector<cv::KeyPoint> keypointsB;
 
     //auto akaze = cv::AKAZE::create();
-    auto akaze = cv::ORB::create(5000);
+    auto orb = cv::ORB::create(5000);
 
-    auto fast = cv::FastFeatureDetector::create();
-//    auto brief = cv::BriefDescriptorExtractor::create();
+    auto fast = cv::FastFeatureDetector::create(5, true, cv::FastFeatureDetector::TYPE_7_12);
+    //auto brief = cv::BriefDescriptorExtractor::create();
 
     CPUStopWatch timer;
-#if 1
+#if 0
     for (unsigned i = 0; i < 10; i++) {
         akaze->detectAndCompute(imgA, cv::noArray(), keypointsA, descsA);
         akaze->detectAndCompute(imgB, cv::noArray(), keypointsB, descsB);
@@ -760,11 +1187,13 @@ int main() {
 #else
     for (unsigned i = 0; i < 10; i++) {
         fast->detect(imgA, keypointsA);
-     //   brief->compute(imgA, keypointsA, descsA);
+        orb->compute(imgA, keypointsA, descsA);
         fast->detect(imgB, keypointsB);
-     //   brief->compute(imgB, keypointsB, descsB);
+        orb->compute(imgB, keypointsB, descsB);
+     /*
         akaze->compute(imgA, keypointsA, descsA);
         akaze->compute(imgB, keypointsB, descsB);
+    */
     }
 #endif
     std::cout << timer.getNanoseconds() * 1e-9f / 20 << " seconds/image" << std::endl;
@@ -788,8 +1217,13 @@ int main() {
     for (auto &v : matches) {
         auto &first = v[0];
         auto &second = v[1];
+        #if 0
         if (thresh * second.distance > first.distance)
             filteredMatches.push_back(v.front());
+        #else
+        if (first.distance < 50)
+            filteredMatches.push_back(v.front());
+        #endif
     }
 
 
